97030e6406a125fd9fec82182adc65f870887032
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2024-11-15 16:04:39 +0200

    deleted old versions of exercises

--- part02-old/phonebook/src/components/Person.js
+++
@@ -1,8 +0,0 @@
-const Person = ({ person, remove }) => {
-    return(
-      <p>
-        {person.name} {person.number} <button onClick={() => remove(person.id)}>delete</button>
-      </p>
-    )}
-
-  export default Person
--- part03-old/README.md
+++
@@ -1,3 +0,0 @@
-### Application created in part 3
-
-Click [here](https://part3-phonebook-vaskivuori.fly.dev/) to see my application.
