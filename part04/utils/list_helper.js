5f659aff7befab022ea440b10b19fd50e8e04d5f
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2024-11-18 13:38:48 +0200

    exercise 4.5 done

--- part04/utils/list_helper.js
+++ part04/utils/list_helper.js
@@ -6,7 +6,24 @@ const totalLikes = (blogs) => {
   return blogs.reduce((accumulator, blog) => accumulator + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  const favorite = blogs.reduce((prev, current) => {
+    return (prev.likes > current.likes) ? prev : current;
+  });
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  };
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
