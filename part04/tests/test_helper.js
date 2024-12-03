d45403402330d7dbf2d89f87a2a9efa43597429a
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2024-12-03 21:13:50 +0200

    it is possible to add user

--- part04/tests/test_helper.js
+++ part04/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog');
+const User = require('../models/user')
 
 const initialBlogs =[
   {
@@ -51,19 +52,16 @@ const initialBlogs =[
   }
 ];
 
-const nonExistingId = async () => {
-  const blog = new Blog({ content: 'willremovethissoon' })
-  await blog.save()
-  await blog.deleteOne()
-
-  return blog._id.toString()
-}
+const usersInDb = async () => {
+  const users = await User.find({});
+  return users.map(u => u.toJSON());
+};
 
 const blogsInDb = async () => {
-  const blogs = await Blog.find({})
-  return blogs.map(blog => blog.toJSON())
-}
+  const blogs = await Blog.find({});
+  return blogs.map(blog => blog.toJSON());
+};
 
 module.exports = {
-  initialBlogs, nonExistingId, blogsInDb
-}
+  usersInDb, initialBlogs, blogsInDb
+};
