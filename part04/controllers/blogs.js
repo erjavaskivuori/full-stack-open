7f95c766d1b3443cb52e7680fad82bfd2100fe6a
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2024-12-04 20:22:17 +0200

    lint

--- part04/controllers/blogs.js
+++ part04/controllers/blogs.js
@@ -1,15 +1,15 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
 const getTokenFrom = request => {
-  const authorization = request.get('authorization')
+  const authorization = request.get('authorization');
   if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
+    return authorization.replace('Bearer ', '');
   }
-  return null
-}
+  return null;
+};
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -19,11 +19,11 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body;
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
   if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
+    return response.status(401).json({ error: 'token invalid' });
   }
-  const user = await User.findById(decodedToken.id)
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     title: body.title,
