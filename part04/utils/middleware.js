ced5067861bf959709292f44f2d9aa08c6dd840e
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-03-17 11:29:25 +0200

    4.20, extracting the token in middleware

--- part04/utils/middleware.js
+++ part04/utils/middleware.js
@@ -1,3 +1,4 @@
+const { request } = require('../app');
 const logger = require('./logger');
 
 const requestLogger = (request, response, next) => {
@@ -28,8 +29,19 @@ const errorHandler = (error, request, response, next) => {
   next(error);
 };
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '');
+  } else {
+    request.token = null;
+  }
+  next();
+}
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 };
