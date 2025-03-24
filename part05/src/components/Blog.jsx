486b4f7cc5b19af37007b44613c1bd4691094f83
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-03-24 19:17:01 +0200

    added button to view all blog info

--- part05/src/components/Blog.jsx
+++ part05/src/components/Blog.jsx
@@ -1,4 +1,13 @@
+import { useState } from 'react'
+
 const Blog = ({ blog }) => {
+  const [blogVisible, setBlogVisible] = useState(false)
+
+  const hideWhenVisible = { display: blogVisible ? 'none' : '' }
+  const showWhenVisible = { display: blogVisible ? '' : 'none' }
+
+  console.log(blog)
+
   const blogStyle = {
     padding: 5,
     paddingLeft: 10,
@@ -10,6 +19,15 @@ const Blog = ({ blog }) => {
   return (
   <div style={blogStyle}>
     <h3>{blog.title} by {blog.author}</h3>
+    <div style={hideWhenVisible}>
+      <button onClick={() => setBlogVisible(true)}>view</button>
+    </div>
+    <div style={showWhenVisible}>
+      <div>{blog.url}</div>
+      <div>likes {blog.likes} <button>like</button></div>
+      <div>{blog.user ? blog.user.name : 'Unknown User'}</div>
+      <button onClick={() => setBlogVisible(false)}>hide</button>
+    </div>
   </div>
 )}
 
