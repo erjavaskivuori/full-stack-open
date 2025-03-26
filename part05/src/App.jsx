d635e5ce1a569956feb96745271c0ef097b133ea
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-03-26 12:10:39 +0200

    5.10, added sorting by likes

--- part05/src/App.jsx
+++ part05/src/App.jsx
@@ -126,7 +126,7 @@ const App = () => {
         <br />
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} addLike={addLike} />
-        )}
+        ).sort((a, b) => b.props.blog.likes - a.props.blog.likes)}
       </div>
     </div>
   )
