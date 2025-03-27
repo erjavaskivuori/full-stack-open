c95115fb7a614dddc1f1bcb1b6ee24f12d0a6bbf
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-03-27 11:26:31 +0200

    formatted

--- part05/src/components/Blog.test.jsx
+++ part05/src/components/Blog.test.jsx
@@ -1,6 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import Blog from './Blog';
+import Blog from './Blog'
 
 const blog = {
   title: 'Testing is important',
@@ -11,49 +11,49 @@ const blog = {
     username: 'ada',
     name: 'Ada Lovelace',
   },
-};
+}
 
 const blogUser = {
   username: 'ada',
   name: 'Ada Lovelace',
-};
+}
 
 describe('<Blog />', () => {
   test('only title and author are rendered initially', () => {
-    render(<Blog blog={blog} user={blogUser} />);
+    render(<Blog blog={blog} user={blogUser} />)
 
-    expect(screen.queryByText('Testing is important by Bob Tester')).toBeInTheDocument();
-    expect(screen.queryByText('https://example.com')).not.toBeInTheDocument();
-    expect(screen.queryByText('likes 42')).not.toBeInTheDocument();
-    expect(screen.queryByText('Ada Lovelace')).not.toBeInTheDocument();
-  });
+    expect(screen.queryByText('Testing is important by Bob Tester')).toBeInTheDocument()
+    expect(screen.queryByText('https://example.com')).not.toBeInTheDocument()
+    expect(screen.queryByText('likes 42')).not.toBeInTheDocument()
+    expect(screen.queryByText('Ada Lovelace')).not.toBeInTheDocument()
+  })
 
   test('url, likes and user are rendered after clicking the view button', async () => {
-    render(<Blog blog={blog} user={blogUser} />);
+    render(<Blog blog={blog} user={blogUser} />)
 
-    const user = userEvent.setup();
-    const button = screen.getByText('view');
-    await user.click(button);
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
 
-    expect(screen.queryByText('Testing is important by Bob Tester')).toBeInTheDocument();
-    expect(screen.queryByText('https://example.com')).toBeInTheDocument();
-    expect(screen.queryByText('likes 42')).toBeInTheDocument();
-    expect(screen.queryByText('Ada Lovelace')).toBeInTheDocument();
-  });
+    expect(screen.queryByText('Testing is important by Bob Tester')).toBeInTheDocument()
+    expect(screen.queryByText('https://example.com')).toBeInTheDocument()
+    expect(screen.queryByText('likes 42')).toBeInTheDocument()
+    expect(screen.queryByText('Ada Lovelace')).toBeInTheDocument()
+  })
 
   test('clicking the like button twice calls the event handler twice', async () => {
-    const addLike = vi.fn();
+    const addLike = vi.fn()
 
-    render(<Blog blog={blog} user={blogUser} addLike={addLike} />);
+    render(<Blog blog={blog} user={blogUser} addLike={addLike} />)
 
-    const user = userEvent.setup();
-    const viewButton = screen.getByText('view');
-    await user.click(viewButton);
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
 
-    const likeButton = screen.getByText('like');
-    await user.click(likeButton);
-    await user.click(likeButton);
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
 
-    expect(addLike.mock.calls).toHaveLength(2);
-  });
-});
+    expect(addLike.mock.calls).toHaveLength(2)
+  })
+})
--- part05/src/components/BlogForm.test.jsx
+++ part05/src/components/BlogForm.test.jsx
@@ -1,31 +1,31 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import BlogForm from './BlogForm';
+import BlogForm from './BlogForm'
 
 const blog = {
   title: 'Testing is important',
   author: 'Bob Tester',
   url: 'https://example.com'
-};
+}
 
 describe('<BlogForm />', () => {
   test('the form calls the event handler with the right details when a new blog is created', async () => {
-    const user = userEvent.setup();
-    const createBlog = vi.fn();
+    const user = userEvent.setup()
+    const createBlog = vi.fn()
 
-    render(<BlogForm createBlog={createBlog} />);
+    render(<BlogForm createBlog={createBlog} />)
 
-    const titleInput = screen.getByLabelText('title');
-    const authorInput = screen.getByLabelText('author');
-    const urlInput = screen.getByLabelText('url');
-    const createButton = screen.getByText('create');
+    const titleInput = screen.getByLabelText('title')
+    const authorInput = screen.getByLabelText('author')
+    const urlInput = screen.getByLabelText('url')
+    const createButton = screen.getByText('create')
 
-    await user.type(titleInput, blog.title);
-    await user.type(authorInput, blog.author);
-    await user.type(urlInput, blog.url);
-    await user.click(createButton);
+    await user.type(titleInput, blog.title)
+    await user.type(authorInput, blog.author)
+    await user.type(urlInput, blog.url)
+    await user.click(createButton)
 
-    expect(createBlog.mock.calls).toHaveLength(1);
-    expect(createBlog.mock.calls[0][0]).toEqual(blog);
-  });
-});
+    expect(createBlog.mock.calls).toHaveLength(1)
+    expect(createBlog.mock.calls[0][0]).toEqual(blog)
+  })
+})
