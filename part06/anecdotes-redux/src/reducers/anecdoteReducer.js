f1c8bbf805eb75ca2fe3741595fcbd3c579d9040
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-04-02 11:54:14 +0300

    6.3, anecdotes can be voted and votes are stored in a Redux store

--- part06/anecdotes-redux/src/reducers/anecdoteReducer.js
+++ part06/anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -23,7 +23,23 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
+  switch (action.type) {
+    case 'VOTE':
+      const id = action.data.id
+      const anecdoteToVote = state.find(a => a.id === id)
+      const votedAnecdote = { ...anecdoteToVote, votes: anecdoteToVote.votes + 1 }
+      return state.map(anecdote =>
+        anecdote.id !== id ? anecdote : votedAnecdote
+      )
+  }
   return state
+}
+
+export const voteAnecdote = (id) => {
+  return {
+    type: 'VOTE',
+    data: { id }
+  }
 }
 
 export default reducer
