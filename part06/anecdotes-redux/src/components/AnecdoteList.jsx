1cced61e1c0a17d98f80ec8ab40d139b1b07c08e
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-04-07 10:29:04 +0300

    6.9, created component for filtering and combined reducer for anecdotes and the filter

--- part06/anecdotes-redux/src/components/AnecdoteList.jsx
+++ part06/anecdotes-redux/src/components/AnecdoteList.jsx
@@ -2,7 +2,12 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(({ filter, anecdotes }) => {
+    if (filter === 'ALL') {
+      return anecdotes
+    }
+    return anecdotes.filter(anecdote => anecdote.content.toLocaleLowerCase().includes(filter.toLocaleLowerCase()))
+  })
   anecdotes.sort((a, b) => b.votes - a.votes)
   const dispatch = useDispatch()
 
