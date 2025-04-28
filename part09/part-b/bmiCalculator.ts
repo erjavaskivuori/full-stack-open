e6c1f8bd1f6b26ce494be3c39869b389046b25a3
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-04-28 19:26:47 +0300

    9.5, bmi calculator can be used from command line and endpoint#

--- part09/part-b/bmiCalculator.ts
+++ part09/part-b/bmiCalculator.ts
@@ -1,41 +1,49 @@
 interface BmiValues {
-  weight: number,
-  height: number
+  weight: number;
+  height: number;
 }
 
 const parseArguments = (args: string[]): BmiValues => {
   if (args.length < 4) throw new Error('Not enough arguments');
   if (args.length > 4) throw new Error('Too many arguments');
+  return validateArguments(args[2], args[3]);
+};
 
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+export const validateArguments = (
+  weight: unknown,
+  height: unknown
+): BmiValues => {
+  if (!isNaN(Number(height)) && !isNaN(Number(weight))) {
     return {
-      weight: Number(args[2]),
-      height: Number(args[3])
-    }
+      weight: Number(weight),
+      height: Number(height)
+    };
   } else {
     throw new Error('Provided values were not numbers!');
   }
-}
+};
 
-
-const calculateBmi = (a: number, b: number): void => {
+export const calculateBmi = (a: number, b: number): string => {
   const bmi = b / (a / 100) ** 2;
   if (bmi < 25) {
-    console.log('Normal range');
+    return 'Normal range';
   } else if (bmi < 29) {
-    console.log('Overweight');
+    return 'Overweight';
   } else {
-    console.log('Obese');
+    return 'Obese';
   }
 };
 
-try {
-  const { weight, height } = parseArguments(process.argv);
-  calculateBmi(weight, height);
-} catch (error: unknown) {
-  let errorMessage = ' An error occured.'
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message;
+if (require.main === module) {
+  try {
+    const { weight, height } = parseArguments(process.argv);
+    const result = calculateBmi(weight, height);
+    console.log(result);
+  } catch (error: unknown) {
+    let errorMessage = ' An error occured.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
   }
-  console.log(errorMessage);
 }
--- part09/part-b/index.ts
+++ part09/part-b/index.ts
@@ -1,8 +1,24 @@
 import express from 'express';
 const app = express();
+import { validateArguments, calculateBmi } from './bmiCalculator';
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const { height, weight } = req.query;
+
+  try {
+    const { height: validHeight, weight: validWeight } = validateArguments(
+      weight,
+      height
+    );
+    const result = calculateBmi(validHeight, validWeight);
+    res.json({ height: validHeight, weight: validWeight, bmi: result });
+  } catch (error: unknown) {
+    res.status(400).json({ error: (error as Error).message });
+  }
 });
 
 const PORT = 3003;
