38f24092f5a6a5cf795bb0819a0cf20cc32a6f90
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-09-03 16:16:12 +0300

    pongrouter:D

--- part09/part-c/src/index.ts
+++ part09/part-c/src/index.ts
@@ -1,15 +1,14 @@
 import express from 'express';
-const app = express();
-app.use(express.json());
 import cors from 'cors';
+import pongRouter from './routes/pong';
+const app = express();
+
+app.use(express.json());
 app.use(cors());
 
 const PORT = 3001;
 
-app.get('/api/ping', (_req, res) => {
-  console.log('someone pinged here');
-  res.send('pong');
-});
+app.use('/api/ping', pongRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
